fac1d15af4d040358ca5b0e56300d01de7e14311
Developer Silo 👨‍💻 <97414850+siloneedsleep@users.noreply.github.com>
2026-06-16 11:29:39 +0700

    Create keep_alive.py

---
+++ keep_alive.py
@@ -0,0 +1,18 @@
+from flask import Flask
+from threading import Thread
+import os
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Equinox Network is Alive!"
+
+def run():
+    # Render luôn cấp một cổng ngầm qua biến PORT, nếu không có thì mặc định chạy 8080
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host='0.0.0.0', port=port)
+
+def keep_alive():
+    t = Thread(target=run)
+    t.start()
